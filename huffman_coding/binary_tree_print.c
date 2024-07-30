3edb86f066c0b19c5a0e1026aae507f4fd71b223
O-01 <sa.an-s@yandex.com>
2024-07-30 14:12:00 -0500

    Saving progress on huffman_codes - local testing successful (huffman_coding)

--- huffman_coding/binary_tree_print.c
+++ huffman_coding/binary_tree_print.c
@@ -19,7 +19,7 @@
 static int print_t(const binary_tree_node_t *tree, int offset, int depth,
 	char **s, int (*print_data)(char *, void *))
 {
-	char b[6];
+	char b[7];
 	int width, left, right, is_left, i;
 
 	if (!tree)
--- huffman_coding/huffman_codes.c
+++ huffman_coding/huffman_codes.c
@@ -1,15 +1,55 @@
 #include "huffman.h"
 
+static void codes_generator(binary_tree_node_t *node, size_t route);
+
 /**
- * huffman_codes -
- * @data:
- * @freq:
- * @size:
- * Return:
+ * huffman_codes - builds Huffman tree and prints resulting Huffman codes for
+ *                 each symbol
+ * @data: char array of size size
+ * @freq: array of associated frequencies of size size
+ * @size: size of data and freq arrays
+ * Return: 1 upon success, otherwise 0
  */
 int huffman_codes(char *data, size_t *freq, size_t size)
 {
+	binary_tree_node_t *huffman = NULL;
+	size_t route = 0;
+
 	if (!data || !freq || !size)
 		return (0);
-	return (0);
+	huffman = huffman_tree(data, freq, size);
+	if (!huffman)
+		return (0);
+	codes_generator(huffman, route);
+	return (1);
+}
+
+/**
+ * codes_generator - evaluates and prints routes to each member of huffman tree
+ * @node: pointer to current node in traversal sequence
+ * @route: number representing necessary steps of traversal in order to
+ *         reach current node. 0 bit = left step, 1 bit = right step
+ */
+static void codes_generator(binary_tree_node_t *node, size_t route)
+{
+	char node_char = 0, hit = 0;
+	size_t mask = 0;
+
+	if (!node)
+		return;
+	node_char = ((symbol_t *)node->data)->data;
+	if (ISLEAF(node) && node_char != -1)
+	{
+		printf("%c: ", node_char);
+		for (mask = 1UL << 63; mask; mask >>= 1)
+		{
+			if ((mask & route) || hit)
+				putchar((mask & route) ? '1' : '0'), hit = 1;
+			else if (!(mask >> 1) && !hit)
+				putchar('0');
+		}
+		putchar('\n');
+	}
+	codes_generator(node->left, route << 1);
+	codes_generator(node->right, (route << 1) + 1);
 }
