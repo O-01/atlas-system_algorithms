b2eae1c00fb6b32698bd46fc1fdcd5a9eed4fe82
O-01 <sa.an-s@yandex.com>
2024-07-30 02:40:52 -0500

    Added huffman_codes - incomplete (huffman_coding)

---
+++ huffman_coding/9-main.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "heap.h"
+#include "huffman.h"
+
+void binary_tree_print(const binary_tree_node_t *heap, int (*print_data)(char *, void *));
+
+/**
+ * symbol_print - Prints a symbol structure
+ *
+ * @buffer: Buffer to print into
+ * @data: Pointer to a node's data
+ *
+ * Return: Number of bytes written in buffer
+ */
+int symbol_print(char *buffer, void *data)
+{
+    symbol_t *symbol;
+    char c;
+    int length;
+
+    symbol = (symbol_t *)data;
+    c = symbol->data;
+    if (c == -1)
+        c = '$';
+    length = sprintf(buffer, "(%c/%lu)", c, symbol->freq);
+    return (length);
+}
+
+/**
+ * nested_print - Prints a symbol structure stored in a nested node
+ *
+ * @buffer: Buffer to print into
+ * @data: Pointer to a node's data
+ *
+ * Return: Number of bytes written in buffer
+ */
+int nested_print(char *buffer, void *data)
+{
+    binary_tree_node_t *nested;
+    symbol_t *symbol;
+    char c;
+    int length;
+
+    nested = (binary_tree_node_t *)data;
+    symbol = (symbol_t *)nested->data;
+    c = symbol->data;
+    if (c == -1)
+        c = '$';
+    length = sprintf(buffer, "(%c/%lu)", c, symbol->freq);
+    return (length);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+    char data[] = {
+        'a', 'b', 'c', 'd', 'e', 'f'
+    };
+    size_t freq[] = {
+        6, 11, 12, 13, 16, 36
+    };
+    size_t size = sizeof(data) / sizeof(data[0]);
+	heap_t *priority_queue = NULL;
+	binary_tree_node_t *root = NULL;
+
+    priority_queue = huffman_priority_queue(data, freq, size);
+    if (!priority_queue)
+    {
+        fprintf(stderr, "Failed to create priority queue\n");
+        return (EXIT_FAILURE);
+    }
+    binary_tree_print(priority_queue->root, nested_print);
+    printf("\n");
+
+    root = huffman_tree(data, freq, size);
+    if (!root)
+    {
+        fprintf(stderr, "Failed to build Huffman tree\n");
+        return (EXIT_FAILURE);
+    }
+    binary_tree_print(root, symbol_print);
+
+
+    if (!huffman_codes(data, freq, size))
+    {
+        fprintf(stderr, "Failed to get Huffman codes\n");
+        return (EXIT_FAILURE);
+    }
+
+    return (EXIT_SUCCESS);
+}
---
+++ huffman_coding/huffman_codes.c
@@ -0,0 +1,15 @@
+#include "huffman.h"
+
+/**
+ * huffman_codes -
+ * @data:
+ * @freq:
+ * @size:
+ * Return:
+ */
+int huffman_codes(char *data, size_t *freq, size_t size)
+{
+	if (!data || !freq || !size)
+		return (0);
+	return (0);
+}
