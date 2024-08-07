8019a329163fb765289a877bb456b799085e350d
O-01 <sa.an-s@yandex.com>
2024-08-07 11:25:30 -0500

    Modified backtracking_graph per Betty formatting guidelines (pathfinding)

--- pathfinding/1-backtracking_graph.c
+++ pathfinding/1-backtracking_graph.c
@@ -4,12 +4,14 @@ static queue_t *backtrack(
 	vertex_t **visit, vertex_t *current, const vertex_t *target, queue_t *q);
 static int is_visited(vertex_t **visit, vertex_t *current);
 
-static int visited;
-
 /**
- * backtracking_graph -
- * @
- * Return:
+ * backtracking_graph - searches for first path from starting point to target
+ *                      point in graph
+ * @graph: pointer to graph to go through
+ * @start: pointer to starting vertex
+ * @target: pointer to target vertex
+ * Return: pointer to queue in which each node is char * corresponding to
+ *         vertex, forming path from start to target
  */
 queue_t *backtracking_graph(
 	graph_t *graph, vertex_t const *start, vertex_t const *target)
@@ -24,16 +26,25 @@ queue_t *backtracking_graph(
 		return (NULL);
 	visit = calloc(graph->nb_vertices + 1, sizeof(vertex_t *));
 	if (!visit)
-		return(queue_delete(q), NULL);
+		return (queue_delete(q), NULL);
 	if (!backtrack(visit, (vertex_t *)&start->index, target, q))
-		queue_delete(q), puts("0--------------------------\n"), q = NULL;
+		queue_delete(q), q = NULL;
 	free(visit), visit = NULL;
 	return (q);
 }
 
+/**
+ * backtrack - backtrack through graph vertices recursively, adding to queue
+ * @visit: group of vertices already visited
+ * @current: vertex currently being processed
+ * @target: vertex as expected end point on route
+ * @q: queue to front of which vertex content added as applicable
+ * Return: pointer to queue upon successful push to front, otherwise NULL
+ */
 static queue_t *backtrack(
 	vertex_t **visit, vertex_t *current, const vertex_t *target, queue_t *q)
 {
+	static int visited;
 	char *content = NULL;
 	edge_t *edge = NULL;
 
@@ -55,6 +66,12 @@ static queue_t *backtrack(
 	return (NULL);
 }
 
+/**
+ * is_visited - checks whether input vertex has already been visited
+ * @visit: group of pointers to vertices already visited
+ * @current: vertex to be verified against group of vertices already visited
+ * Return: 1 if vertex has already been visited, otherwise 0
+ */
 static int is_visited(vertex_t **visit, vertex_t *current)
 {
 	vertex_t **vertex = NULL;
