7affbeac71cf5fd5629f4708e86a461bbee3db79
O-01 <sa.an-s@yandex.com>
2024-08-06 23:49:19 -0500

    Saving progress on backtracking_graph - seemingly almost finished (pathfinding)

--- pathfinding/1-backtracking_graph.c
+++ pathfinding/1-backtracking_graph.c
@@ -1,6 +1,10 @@
 #include "pathfinding.h"
 
-queue_t *backtrack(graph_t *graph, vertex_t *current, vertex_t *target);
+static queue_t *backtrack(
+	vertex_t **visit, vertex_t *current, const vertex_t *target, queue_t *q);
+static int is_visited(vertex_t **visit, vertex_t *current);
+
+static int visited;
 
 /**
  * backtracking_graph -
@@ -13,13 +17,50 @@ queue_t *backtracking_graph(
 	queue_t *q = NULL;
 	vertex_t **visit = NULL;
 
-	if (!graph || graph->vertices || !start || !target)
+	if (!graph || !graph->vertices || !start || !target)
 		return (NULL);
 	q = queue_create();
 	if (!q)
 		return (NULL);
-	visit = calloc(1, graph->nb_vertices * sizeof(vertex_t *));
+	visit = calloc(graph->nb_vertices + 1, sizeof(vertex_t *));
 	if (!visit)
-		return (queue_delete(q), NULL);
-	;
+		return(queue_delete(q), NULL);
+	if (!backtrack(visit, (vertex_t *)&start->index, target, q))
+		queue_delete(q), puts("0--------------------------\n"), q = NULL;
+	free(visit), visit = NULL;
+	return (q);
+}
+
+static queue_t *backtrack(
+	vertex_t **visit, vertex_t *current, const vertex_t *target, queue_t *q)
+{
+	char *content = NULL;
+	edge_t *edge = NULL;
+
+	if (!current || (!current->edges && current != target) ||
+		is_visited(visit, current))
+		return (NULL);
+	visit[visited++] = current;
+	printf("Checking %s\n", current->content);
+	for (edge = current->edges; edge; edge = edge->next)
+		if (current == target || backtrack(visit, edge->dest, target, q))
+		{
+			content = calloc(1, strlen(current->content) + 1);
+			if (!content)
+				return (NULL);
+			content = current->content;
+			queue_push_front(q, content);
+			return (q);
+		}
+	return (NULL);
+}
+
+static int is_visited(vertex_t **visit, vertex_t *current)
+{
+	vertex_t **vertex = NULL;
+
+	for (vertex = visit; *vertex; ++vertex)
+		if (*vertex == current)
+			return (1);
+	return (0);
 }
