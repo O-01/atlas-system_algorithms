29f4801e7a3d4bbafa99cd8ded2b77e225376b82
O-01 <sa.an-s@yandex.com>
2024-08-08 09:49:00 -0500

    Modified dijkstra_graph per Betty formatting guidelines, cleaned code for improved readability (pathfinding)

--- pathfinding/2-dijkstra_graph.c
+++ pathfinding/2-dijkstra_graph.c
@@ -9,20 +9,23 @@ static int get_path(queue_t **, const vertex_t *, vertex_t *, vertex_t **);
 static void free_stuff(vertex_t **, vertex_t **, int *, int *);
 
 /**
- * dijkstra_graph -
- * @graph:
- * @start:
- * @target:
- * Return:
+ * dijkstra_graph - searches for shortest path from starting point to target
+ *                  point in graph
+ * @graph: pointer to graph to go through
+ * @start: pointer to starting vertex
+ * @target: pointer to target vertex
+ * Return: queue data structure containing points representing shortest path
+ *         from starting point to target point upon success, otherwise NULL
+ *         pointer
  */
 queue_t *dijkstra_graph(
 	graph_t *graph, vertex_t const *start, vertex_t const *target)
 {
 	queue_t *q = NULL;
-	vertex_t *current = NULL, **vertices = NULL, **route = NULL;
-	edge_t *edge = NULL;
 	int *visit = NULL, *dist = NULL, current_dist = 0;
-	size_t i = 0;
+	vertex_t **vertices = NULL, **route = NULL, *current = NULL;
+	size_t iter = 0;
+	edge_t *edge = NULL;
 
 	if (!graph || !graph->vertices || !start || !target)
 		return (NULL);
@@ -30,12 +33,11 @@ queue_t *dijkstra_graph(
 		return (NULL);
 	dist[start->index] = 0;
 	route[start->index] = (vertex_t *)&start->index;
-	for (; current != target;)
+	for (current_dist = BIG; current != target; current_dist = BIG)
 	{
-		current_dist = BIG;
-		for (current = NULL, i = 0; i < graph->nb_vertices; ++i)
-			if (!visit[i] && dist[i] < current_dist)
-				current = vertices[i], current_dist = dist[current->index];
+		for (current = NULL, iter = 0; iter < graph->nb_vertices; ++iter)
+			if (!visit[iter] && dist[iter] < current_dist)
+				current = vertices[iter], current_dist = dist[current->index];
 		if (!current)
 			break;
 		printf("Checking %s, distance from %s is %d\n",
@@ -52,6 +54,18 @@ queue_t *dijkstra_graph(
 	return (q);
 }
 
+/**
+ * prep - allocates memory for data structures to be used in dijkstra_graph
+ * @q: queue data structure, record of shortest path from A to B
+ * @visit: pointer to integer array, record of visited/processed vertices
+ * @dist: pointer to integer array, record of distances from start point
+ * @vertices: pointer to array of pointers to vertices, serialization of all
+ *            vertices within graph ordered by index
+ * @route: pointer to array of pointers to vertices, serialization of vertices
+ *         composing route from start to target, as applicable
+ * @graph: pointer to graph to go through
+ * Return: 1 upon total success, otherwise 0 upon error
+ */
 static int prep(queue_t **q, int **visit, int **dist,
 	vertex_t ***vertices, vertex_t ***route, graph_t *graph)
 {
@@ -79,6 +93,16 @@ static int prep(queue_t **q, int **visit, int **dist,
 	return (1);
 }
 
+/**
+ * get_path - enqueue content sequentially from points on route composing
+ *            shortest path from A to B
+ * @q: queue data structure, record of shortest path from A to B
+ * @start: pointer to starting vertex
+ * @current: pointer to vertex, assumed to be pointer to target vertex
+ * @route: pointer to array of pointers to vertices, serialization of vertices
+ *         composing route from start to target, as applicable
+ * Return: 1 upon success, otherwise 0 upon error
+ */
 static int get_path(
 	queue_t **q, const vertex_t *start, vertex_t *current, vertex_t **route)
 {
@@ -100,6 +124,15 @@ static int get_path(
 	return (1);
 }
 
+/**
+ * free_stuff - frees memory allocated within dijkstra_graph
+ * @route: pointer to array of pointers to vertices, serialization of vertices
+ *         composing route from start to target, as applicable
+ * @vertices: pointer to array of pointers to vertices, serialization of all
+ *            vertices within graph ordered by index
+ * @dist: pointer to integer array, record of distances from start point
+ * @visit: pointer to integer array, record of visited/processed vertices
+ */
 static void free_stuff(
 	vertex_t **route, vertex_t **vertices, int *dist, int *visit)
 {
