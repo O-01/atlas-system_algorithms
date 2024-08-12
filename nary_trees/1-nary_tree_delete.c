4d866cad87bf8ede943e034a16d93c9caedfa9fa
O-01 <sa.an-s@yandex.com>
2024-08-12 13:10:43 -0500

    Saving progress on nary_tree_delete - local testing successful (nary_trees)

--- nary_trees/1-nary_tree_delete.c
+++ nary_trees/1-nary_tree_delete.c
@@ -1,9 +1,14 @@
 #include "nary_trees.h"
 
 /**
- * 
+ * nary_tree_delete - deallocates entire N-ary tree
+ * @tree: N-ary tree to be deleted
  */
 void nary_tree_delete(nary_tree_t *tree)
 {
-	;
+	if (!tree)
+		return;
+	nary_tree_delete(tree->children);
+	nary_tree_delete(tree->next);
+	FREE_0(tree->content), FREE_0(tree);
 }
