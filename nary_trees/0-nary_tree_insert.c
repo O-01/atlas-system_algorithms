d171a9ccdefa03060e63efee86b37e31c04dd601
O-01 <sa.an-s@yandex.com>
2024-08-12 12:51:12 -0500

    Saving progress on nary_tree_insert - local testing successful (nary_trees)

--- nary_trees/0-nary_tree_insert.c
+++ nary_trees/0-nary_tree_insert.c
@@ -1,9 +1,26 @@
 #include "nary_trees.h"
 
 /**
- * 
+ * nary_tree_insert - inserts node into N-ary tree
+ * @parent: pointer to parent node
+ * @str: string to be stored in created node, must be duplicated
+ * Return: pointer to created node upon success, otherwise NULL;
  */
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 {
-	;
+	nary_tree_t *add = NULL;
+
+	add = calloc(1, sizeof(nary_tree_t));
+	if (!add)
+		return (NULL);
+	add->content = strdup(str);
+	add->parent = parent;
+	add->nb_children = 0;
+	add->children = NULL;
+	if (!parent)
+		return (add->next = NULL, add);
+	add->next = parent->children;
+	parent->children = add;
+	++parent->nb_children;
+	return (add);
 }
--- nary_trees/nary_trees.h
+++ nary_trees/nary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define FREE_0(x) (free((x)), (x) = NULL)
 
